7698256671007d741b5fdd70d0a3d9b58a9abbab
Bowen <948266921@qq.com>
2020-11-09 19:23:25 +0800

    create backtrackind

--- src/Algorithm/Sort/SleepSort.ts
+++ src/Algorithm/Sort/SleepSort.ts
@@ -1,7 +1,8 @@
 // 睡眠排序
 // 天才排序算法：睡眠排序
 
-function sleepSort<T>(arr: T[]){
+// 该方法直接return result是undefined的，因为会有延时。解决：promise包装
+function sleepSort<T>(arr: T[]) {
   const result: T[] = [];
   for (let i = 0; i < arr.length; i++) {
     setTimeout(() => {
@@ -13,5 +14,26 @@ function sleepSort<T>(arr: T[]){
 
 const arr = [ 5, 8, 2, 7, 8, 9, 1, 10, 0 ];
 sleepSort(arr)
+
+
+// 使用promise包装
+function sleepSortByPromise<T>(arr: T[]): void {
+  const result: T[] = [];
+  const promise = new Promise<T[]>((resolve) => {
+    for (let i = 0; i < arr.length; i++) {
+      setTimeout(() => {
+        result.push(arr[i]);
+        if (result.length === arr.length) resolve(result);
+      }, Number(arr[i]));
+    }
+  })
+
+  promise.then((data) => {
+    return data;
+  })
+}
+
+// 一开始没有输出，因此第一次会输出undefined
+console.log(sleepSort(arr));
 
 // 有点意思吧？同学A： 那要是值太大是9999999呢？我：爪巴

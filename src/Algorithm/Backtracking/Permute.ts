6eacf12e7b3539cd2bc70ab13b8afee94b3fbdb7
Bowen <948266921@qq.com>
2020-11-10 00:48:49 +0800

    add permute

--- src/Algorithm/Backtracking/Permute.ts
+++ src/Algorithm/Backtracking/Permute.ts
@@ -1,1 +1,31 @@
-// 
+// 全排列
+// 我们在高中的时候就做过排列组合的数学题，我们也知道 n 个不重复的数，全排列共有 n! 个
+
+
+// 输入一组不重复的数字，返回它们的全排列
+export function permute(nums: number[]) {
+  const res: Array<number[]> = [];
+  const track: number[] = [];
+
+  return backtrack(nums, track, res);
+}
+
+// 辅助函数，也是框架函数
+function backtrack(nums: number[], track: number[], res: Array<number[]>): Array<number[]> | void {
+  if (track.length === nums.length) {
+    res.push(track);
+    return;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    // 这里的复杂度有点高，后面可以尝试优化
+    if (track.includes(nums[i])) continue;
+
+    track.push(nums[i]);
+    backtrack(nums, track, res);
+    track.pop();
+  }
+}
+
+const nums = [1, 2, 3];
+console.log(permute(nums))
--- src/Algorithm/DynamicProgramming/ChangeCoins.ts
+++ src/Algorithm/DynamicProgramming/ChangeCoins.ts
@@ -25,3 +25,90 @@ function coinChange(coins: number[], amount: number): number {
   return dp(amount);
 }
 */
+
+// 备忘录写法(自上而下)
+// 画出递归树的时候会发现有重叠子问题，需要用备忘录之类的降低复杂度
+export function coinMemo(coins: number[], amount: number): number {
+  // 创建备忘录
+  const memo = new Map();
+
+  // 定义：要凑出金额 n，至少要 dp(n) 个硬币, 【确定状态】
+  const dp = (num: number): number => {
+    if (memo.get(num) !== undefined) return memo.get(num);
+
+    // base 基本值判断
+    if (num === 0) return 0;
+    if (num < 0) return -1;
+
+    // 为了判断num - coin有无选择（num - coin >= 0）
+    // 若无选择则通过isFinite判断是否返回-1
+    let res = Infinity;
+
+    // 【确定选择】
+    for (const coin of coins) {
+      const superAmount = dp(num - coin);
+      if (superAmount === -1) continue;
+      
+      res = Math.min(res, 1 + superAmount);
+    }
+
+    if (!isFinite(res)) memo.set(num, -1);
+    else memo.set(num, res);
+
+    return memo.get(num);
+  }
+
+  return dp(amount);
+}
+
+console.log(coinMemo([1, 2, 5], 11));
+
+
+// dp数组的迭代写法(自下而上)
+export function coinDp(coins: number[], amount: number): number {
+  // 数组大小为 amount + 1，初始值也为 amount + 1
+  // dp 数组的定义：当目标金额为 i 时，至少需要 dp[i] 枚硬币凑出。
+  const dp = new Array(amount + 1).fill(amount + 1);
+  // base case
+  dp[0] = 0;
+  // 外层 for 循环在遍历所有状态的所有取值
+  for (let i = 0; i < dp.length; i++) {
+    // 内层 for 循环在求所有选择的最小值
+    for (const coin of coins) {
+      // i - coin >= 0说明有选择
+      if (i - coin < 0) continue;
+      // 如i = 5, coin有三个选择1 2 5
+      // 此时要找dp[4] dp[3] dp[0]之前最少硬币数的值 然后 + 1(所选的硬币)
+      dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+    }
+  }
+
+  return dp[amount] === amount + 1 ? -1 : dp[amount];
+}
+
+console.log(coinDp([1, 2, 5], 11));
+
+
+// 使用dp数组获取最少的硬币数所组成的硬币数组
+export function coinsListDp(coins: number[], amount: number): number[] {
+  // 此时的dp数组包含硬币数量和硬币数组
+  const dp: { count: number; coinsArr: number[]}[] 
+    = Array.from(new Array(amount + 1), () => ({ count: amount + 1, coinsArr: [] }));
+
+  dp[0].count = 0;
+
+  for (let i = 0; i < dp.length; i++) {
+    for (const coin of coins) {
+      if (i - coin < 0) continue;
+      
+      if (dp[i].count > dp[i - coin].count + 1) {
+        dp[i].count = dp[i - coin].count + 1;
+        dp[i].coinsArr = [...dp[i - coin].coinsArr, coin];
+      }
+    }
+  }
+
+  return dp[amount].count === amount + 1 ? [] : dp[amount].coinsArr;
+}
+
+console.log(coinsListDp([1, 2, 5], 11));
